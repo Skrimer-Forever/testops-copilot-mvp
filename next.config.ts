f3fc1d37754ad916f78f37ad80090860657bf133
Skrimer-Forever <gusevaleksej01136@gmail.com>
2025-12-10 15:30:38 +0300

    Add API proxy rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/generation/:path*',
+        destination: 'http://176.123.161.105:8000/generation/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
